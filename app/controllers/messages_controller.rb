3ea658418281717f626ba764fd245807cb293e2a
ryosuke0032 <ryosuke0032@gmail.com>
2015-08-25 12:23:29 +0000

    8.6dousakakunin

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,40 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit,:update]
+  
   def index
     @messages = Message.all
     @message = Message.new
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path, notice: "メッセージを編集しました"
+    else
+      render 'edit'
+    end
+  end
   
   def create
 	@message = Message.new(message_params)
-  if	@message.save
+   if	@message.save
 	redirect_to root_path , notice: 'メッセージを保存しました'
-	else
+	 else
 	  @messages = Message.all
 	  flash.now[:alert] ="メッセージの保存に失敗しました"
 	  render 'index'
-	end
+	 end
   end
 
   private
   def message_params
 	params.require(:message).permit(:name,:body)
   end
-
+  
+  def set_message
+  @message = Message.find(params[:id])
+  end
 
 end
